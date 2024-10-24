35850fab16fec8d25681b017d2798dea38f620ab
IoIxD <alphaproject217@gmail.com>
2024-10-24 14:43:56 -0700

    enums

--- rres-rs/src/rres.rs
+++ rres-rs/src/rres.rs
@@ -1,3 +1,5 @@
+#![allow(non_camel_case_types)]
+
 use std::{
     ffi::{CStr, CString},
     ops::{Deref, DerefMut},
@@ -7,6 +9,155 @@ pub struct ResourceChunk(pub(crate) rres_sys::rresResourceChunk);
 pub struct ResourceMulti(pub(crate) rres_sys::rresResourceMulti);
 pub struct ResourceChunkInfo(pub(crate) rres_sys::rresResourceChunkInfo);
 pub struct CentralDir(pub(crate) rres_sys::rresCentralDir);
+pub struct ResourceChunkData(pub(crate) rres_sys::rresResourceChunkData);
+pub struct FontGlyphInfo(pub(crate) rres_sys::rresFontGlyphInfo);
+
+#[repr(u32)]
+enum ResourceDataType {
+    DATA_NULL = rres_sys::rresResourceDataType::RRES_DATA_NULL as u32,
+    DATA_RAW = rres_sys::rresResourceDataType::RRES_DATA_RAW as u32,
+
+    DATA_TEXT = rres_sys::rresResourceDataType::RRES_DATA_TEXT as u32,
+
+    DATA_IMAGE = rres_sys::rresResourceDataType::RRES_DATA_IMAGE as u32,
+
+    DATA_WAVE = rres_sys::rresResourceDataType::RRES_DATA_WAVE as u32,
+
+    DATA_VERTEX = rres_sys::rresResourceDataType::RRES_DATA_VERTEX as u32,
+
+    DATA_FONT_GLYPHS = rres_sys::rresResourceDataType::RRES_DATA_FONT_GLYPHS as u32,
+
+    DATA_LINK = rres_sys::rresResourceDataType::RRES_DATA_LINK as u32,
+
+    DATA_DIRECTORY = rres_sys::rresResourceDataType::RRES_DATA_DIRECTORY as u32,
+}
+#[repr(u32)]
+enum CompressionType {
+    COMP_NONE = rres_sys::rresCompressionType::RRES_COMP_NONE as u32,
+    COMP_RLE = rres_sys::rresCompressionType::RRES_COMP_RLE as u32,
+    COMP_DEFLATE = rres_sys::rresCompressionType::RRES_COMP_DEFLATE as u32,
+    COMP_LZ4 = rres_sys::rresCompressionType::RRES_COMP_LZ4 as u32,
+    COMP_LZMA2 = rres_sys::rresCompressionType::RRES_COMP_LZMA2 as u32,
+    COMP_QOI = rres_sys::rresCompressionType::RRES_COMP_QOI as u32,
+}
+#[repr(u32)]
+enum EncryptionType {
+    CIPHER_NONE = rres_sys::rresEncryptionType::RRES_CIPHER_NONE as u32,
+    CIPHER_XOR = rres_sys::rresEncryptionType::RRES_CIPHER_XOR as u32,
+    CIPHER_DES = rres_sys::rresEncryptionType::RRES_CIPHER_DES as u32,
+    CIPHER_TDES = rres_sys::rresEncryptionType::RRES_CIPHER_TDES as u32,
+    CIPHER_IDEA = rres_sys::rresEncryptionType::RRES_CIPHER_IDEA as u32,
+    CIPHER_AES = rres_sys::rresEncryptionType::RRES_CIPHER_AES as u32,
+    CIPHER_AES_GCM = rres_sys::rresEncryptionType::RRES_CIPHER_AES_GCM as u32,
+    CIPHER_XTEA = rres_sys::rresEncryptionType::RRES_CIPHER_XTEA as u32,
+    CIPHER_BLOWFISH = rres_sys::rresEncryptionType::RRES_CIPHER_BLOWFISH as u32,
+    CIPHER_RSA = rres_sys::rresEncryptionType::RRES_CIPHER_RSA as u32,
+    CIPHER_SALSA20 = rres_sys::rresEncryptionType::RRES_CIPHER_SALSA20 as u32,
+    CIPHER_CHACHA20 = rres_sys::rresEncryptionType::RRES_CIPHER_CHACHA20 as u32,
+    CIPHER_XCHACHA20 = rres_sys::rresEncryptionType::RRES_CIPHER_XCHACHA20 as u32,
+    CIPHER_XCHACHA20_POLY1305 = rres_sys::rresEncryptionType::RRES_CIPHER_XCHACHA20_POLY1305 as u32,
+}
+#[repr(u32)]
+enum ErrorType {
+    SUCCESS = rres_sys::rresErrorType::RRES_SUCCESS as u32,
+    ERROR_FILE_NOT_FOUND = rres_sys::rresErrorType::RRES_ERROR_FILE_NOT_FOUND as u32,
+    ERROR_FILE_FORMAT = rres_sys::rresErrorType::RRES_ERROR_FILE_FORMAT as u32,
+    ERROR_MEMORY_ALLOC = rres_sys::rresErrorType::RRES_ERROR_MEMORY_ALLOC as u32,
+}
+#[repr(u32)]
+enum TextEncoding {
+    TEXT_ENCODING_UNDEFINED = rres_sys::rresTextEncoding::RRES_TEXT_ENCODING_UNDEFINED as u32,
+    TEXT_ENCODING_UTF8 = rres_sys::rresTextEncoding::RRES_TEXT_ENCODING_UTF8 as u32,
+    TEXT_ENCODING_UTF8_BOM = rres_sys::rresTextEncoding::RRES_TEXT_ENCODING_UTF8_BOM as u32,
+    TEXT_ENCODING_UTF16_LE = rres_sys::rresTextEncoding::RRES_TEXT_ENCODING_UTF16_LE as u32,
+    TEXT_ENCODING_UTF16_BE = rres_sys::rresTextEncoding::RRES_TEXT_ENCODING_UTF16_BE as u32,
+}
+#[repr(u32)]
+enum CodeLang {
+    CODE_LANG_UNDEFINED = rres_sys::rresCodeLang::RRES_CODE_LANG_UNDEFINED as u32,
+    CODE_LANG_C = rres_sys::rresCodeLang::RRES_CODE_LANG_C as u32,
+    CODE_LANG_CPP = rres_sys::rresCodeLang::RRES_CODE_LANG_CPP as u32,
+    CODE_LANG_CS = rres_sys::rresCodeLang::RRES_CODE_LANG_CS as u32,
+    CODE_LANG_LUA = rres_sys::rresCodeLang::RRES_CODE_LANG_LUA as u32,
+    CODE_LANG_JS = rres_sys::rresCodeLang::RRES_CODE_LANG_JS as u32,
+    CODE_LANG_PYTHON = rres_sys::rresCodeLang::RRES_CODE_LANG_PYTHON as u32,
+    CODE_LANG_RUST = rres_sys::rresCodeLang::RRES_CODE_LANG_RUST as u32,
+    CODE_LANG_ZIG = rres_sys::rresCodeLang::RRES_CODE_LANG_ZIG as u32,
+    CODE_LANG_ODIN = rres_sys::rresCodeLang::RRES_CODE_LANG_ODIN as u32,
+    CODE_LANG_JAI = rres_sys::rresCodeLang::RRES_CODE_LANG_JAI as u32,
+    CODE_LANG_GDSCRIPT = rres_sys::rresCodeLang::RRES_CODE_LANG_GDSCRIPT as u32,
+    CODE_LANG_GLSL = rres_sys::rresCodeLang::RRES_CODE_LANG_GLSL as u32,
+}
+#[repr(u32)]
+enum PixelFormat {
+    PIXELFORMAT_UNDEFINED = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNDEFINED as u32,
+    PIXELFORMAT_UNCOMP_GRAYSCALE =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_GRAYSCALE as u32,
+    PIXELFORMAT_UNCOMP_GRAY_ALPHA =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_GRAY_ALPHA as u32,
+    PIXELFORMAT_UNCOMP_R5G6B5 = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R5G6B5 as u32,
+    PIXELFORMAT_UNCOMP_R8G8B8 = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R8G8B8 as u32,
+    PIXELFORMAT_UNCOMP_R5G5B5A1 =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R5G5B5A1 as u32,
+    PIXELFORMAT_UNCOMP_R4G4B4A4 =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R4G4B4A4 as u32,
+    PIXELFORMAT_UNCOMP_R8G8B8A8 =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R8G8B8A8 as u32,
+    PIXELFORMAT_UNCOMP_R32 = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R32 as u32,
+    PIXELFORMAT_UNCOMP_R32G32B32 =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R32G32B32 as u32,
+    PIXELFORMAT_UNCOMP_R32G32B32A32 =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_UNCOMP_R32G32B32A32 as u32,
+    PIXELFORMAT_COMP_DXT1_RGB = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_DXT1_RGB as u32,
+    PIXELFORMAT_COMP_DXT1_RGBA = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_DXT1_RGBA as u32,
+    PIXELFORMAT_COMP_DXT3_RGBA = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_DXT3_RGBA as u32,
+    PIXELFORMAT_COMP_DXT5_RGBA = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_DXT5_RGBA as u32,
+    PIXELFORMAT_COMP_ETC1_RGB = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_ETC1_RGB as u32,
+    PIXELFORMAT_COMP_ETC2_RGB = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_ETC2_RGB as u32,
+    PIXELFORMAT_COMP_ETC2_EAC_RGBA =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_ETC2_EAC_RGBA as u32,
+    PIXELFORMAT_COMP_PVRT_RGB = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_PVRT_RGB as u32,
+    PIXELFORMAT_COMP_PVRT_RGBA = rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_PVRT_RGBA as u32,
+    PIXELFORMAT_COMP_ASTC_4x4_RGBA =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_ASTC_4x4_RGBA as u32,
+    PIXELFORMAT_COMP_ASTC_8x8_RGBA =
+        rres_sys::rresPixelFormat::RRES_PIXELFORMAT_COMP_ASTC_8x8_RGBA as u32,
+}
+#[repr(u32)]
+enum VertexAttribute {
+    VERTEX_ATTRIBUTE_POSITION =
+        rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_POSITION as u32,
+    VERTEX_ATTRIBUTE_TEXCOORD1 =
+        rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_TEXCOORD1 as u32,
+    VERTEX_ATTRIBUTE_TEXCOORD2 =
+        rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_TEXCOORD2 as u32,
+    VERTEX_ATTRIBUTE_TEXCOORD3 =
+        rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_TEXCOORD3 as u32,
+    VERTEX_ATTRIBUTE_TEXCOORD4 =
+        rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_TEXCOORD4 as u32,
+    VERTEX_ATTRIBUTE_NORMAL = rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_NORMAL as u32,
+    VERTEX_ATTRIBUTE_TANGENT = rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_TANGENT as u32,
+    VERTEX_ATTRIBUTE_COLOR = rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_COLOR as u32,
+    VERTEX_ATTRIBUTE_INDEX = rres_sys::rresVertexAttribute::RRES_VERTEX_ATTRIBUTE_INDEX as u32,
+}
+#[repr(u32)]
+enum VertexFormat {
+    VERTEX_FORMAT_UBYTE = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_UBYTE as u32,
+    VERTEX_FORMAT_BYTE = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_BYTE as u32,
+    VERTEX_FORMAT_USHORT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_USHORT as u32,
+    VERTEX_FORMAT_SHORT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_SHORT as u32,
+    VERTEX_FORMAT_UINT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_UINT as u32,
+    VERTEX_FORMAT_INT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_INT as u32,
+    VERTEX_FORMAT_HFLOAT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_HFLOAT as u32,
+    VERTEX_FORMAT_FLOAT = rres_sys::rresVertexFormat::RRES_VERTEX_FORMAT_FLOAT as u32,
+}
+#[repr(u32)]
+enum FontStyle {
+    FONT_STYLE_UNDEFINED = rres_sys::rresFontStyle::RRES_FONT_STYLE_UNDEFINED as u32,
+    FONT_STYLE_REGULAR = rres_sys::rresFontStyle::RRES_FONT_STYLE_REGULAR as u32,
+    FONT_STYLE_BOLD = rres_sys::rresFontStyle::RRES_FONT_STYLE_BOLD as u32,
+    FONT_STYLE_ITALIC = rres_sys::rresFontStyle::RRES_FONT_STYLE_ITALIC as u32,
+}
 
 impl ResourceChunk {
     pub fn new(file_name: &str, id: i32) -> Self {
